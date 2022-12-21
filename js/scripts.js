2307d7a799b75fb778f12a87a2a6a07f5abd4e52
CalendaLucia <calendalucia4@gmail.com>
2022-12-21 16:27:43 +0100

    finito

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,62 @@
 // // Verificare se index è multiplo di 3. Se lo è, stampa FIZZ, altrimenti se è multiplo di 5 stampa BUZZ
-// Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”
 
 
 
-for (let index = 0; index < 100; index++) {
+
+
+for (let index = 1; index <= 100; index++) {
     
-    console.log("index" + (index + 1 + 1));
 
-    if (index % 3 == 0) {
-       
+    
+    if ( (index % 3 == 0) && (index % 5 == 0) ) {  
+
+        const newDiv = document.createElement('div');
+        newDiv.innerHTML = "Fizzbuz";
+        newDiv.className = `box box-${index}`;
+        container.append(newDiv);
+        newDiv.style.backgroundColor = "lawgreen";
+        const color = ["red", "beige", "burlywood", "coral", "aqua", "darkcyan", "pink", "lawngreen", "fuchsia"];
         
-        console.log(index + "siamo multipli di 3");
-    }
-   
-
-     else if ( index % 5 == 0) {
-
-        console.log(index + "siamo multipli di 5");
+        
     }
 
-
-    else if ( index % 15 == 0) {      //CI STO ANCORA RIFLETTENDO
-
-        console.log("SIAMO ROSSI");
-    }
-
-    else  {
-
-        console.log("CIAO")
-    }
-
-
+    else if (index % 3 == 0) {   
+        console.log('Fizz');
+        const newDiv = document.createElement('div');
+        newDiv.innerHTML = "Fizz";
+        newDiv.className = `box box-${index}`;
+        container.append(newDiv);
+        newDiv.style.backgroundColor = "aqua";
     
+    }
+
+    else if (index % 5 == 0) { 
+        console.log('Buz');
+        const newDiv = document.createElement('div');
+        newDiv.innerHTML = "Buz";
+        newDiv.className = `box box-${index}`;
+        container.append(newDiv);
+        newDiv.style.backgroundColor = "darkcyan";
     
+    }
+
+    else {    
+        console.log(index);
+        const newDiv = document.createElement('div');
+        newDiv.innerHTML = `${index}`;
+        newDiv.className = `box box-${index}`;
+        container.append(newDiv);
+        newDiv.style.backgroundColor = "burlywood";
+    
+    }
+
+
+    //AGGIUNTA ELEMENTI NEL DOM
+
+  
+
 }
-
 
 
 
